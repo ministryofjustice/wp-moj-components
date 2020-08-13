826dd01b01566aba6750c8a09607f163e9ee281e
adam <adam.brown@digital.justice.gov.uk>
2020-08-13 14:11:54 +0100

    Improve commenting

--- component/Analytics/AnalyticsSettings.php
+++ component/Analytics/AnalyticsSettings.php
@@ -24,17 +24,20 @@ class AnalyticsSettings extends Analytics
         add_settings_field(
             'gtm_analytics_id',
             __('GTM ID', 'wp-moj-components'),
-            [$this, 'gtmAnalyticsId'],
+            [$this, 'setGoogleTagManagerID'],
             'mojComponentSettings',
             $section
         );
     }
 
-    public function gtmAnalyticsId()
+    /**
+     * Function that collects inputed GTM ID and running checks on it
+     */
+    public function setGoogleTagManagerID()
     {
         $options = get_option('moj_component_settings');
         $googleTagManagerID = $options['gtm_analytics_id'] ?? '';
-
+        
         ?>
         <input type='text' name='moj_component_settings[gtm_analytics_id]'
         placeholder="GTM-XXXXXXX" value='<?php echo sanitize_html_class($googleTagManagerID); ?>' 
